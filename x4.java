a4e04fe85d4704f8b8c9779d8de4288e16bf62c6
Puled81 <drakep@verizon.net>
2015-09-30 19:02:28 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -1,46 +1,40 @@
-//// Exercise 4:  click button to change background.
-
 int r,g,b;
-float button1X=100, button1Y=100, button1W=80, button1H=40;
+float buttonX=200, buttonY=250, buttonW=80, buttonH=40;
 int counter=0;
 
 //// SETUP:  size only.  Also set colors.
 void setup() {
-  size( 640, 480 );
+  size( 500, 500 );
   reset();
 }
 void reset() {
-  r=  100;
-  g=  200;
-  b=  250;
+  r=  255;
+  g=  255;
+  b=  0;
 }
-
-
-//// NEXT:  button only.
 void draw() {
   background( r,g,b );
-  showButton( button1X, button1Y, button1W, button1H );
-  fill(100,0,0);
-  text( "Click me!", button1X+button1W/4, button1Y+button1H*2/3 );
+  showButton( buttonX, buttonY, buttonW, buttonH );
+  fill(255,110,0);
+  text( "Change", buttonX+buttonW/4, buttonY+buttonH*2/3 );
 }
 // Draw the button.
 void showButton( float x, float y, float w, float h ) {
-  fill( 255,255,0 );
+  fill( 0,0,255 );
   rect ( x,y, w,h );
 }
-
-//// HANDLERS:  keys & click
+/// HANDLERS:  keys & click
 void keyPressed() {
   if (key == 'q') exit();
   if (key == 'r') reset();
 }
 void mousePressed() {
-  if ( hit( mouseX,mouseY, 100,100, 50,50 ) ) {
+  if ( hit( mouseX,mouseY, width/2,height/2, 50,50 ) ) {
     counter=  counter+1;
     if (counter % 2 > 0) {
-      r=  255;
-      g=  50;
-      b=  0;
+      r=  0;
+      g=  255;
+      b=  255;
     } else {
       reset();
     }
